cfe6d7a935a90205dd75237c4342be80d82b3248
Adithya <adithyaanilk@gmail.com>
2025-01-03 17:28:33 +0530

    build error fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
-/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/no-page-custom-font */
+ 
 import Head from "next/head";
-import Homepage from "./index/page";
+import Homepage from "./main/page";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Home() {
   return (
     <><Head>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/owl.carousel.min.js" />
-<script src='https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap'/>
-    </Head><div>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/owl.carousel.min.js"
+      async
+    ></script>
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+    
+    <div>
         <Homepage />
 
       </div></>
